137ef7f2046bfbf179c36484f59e69763c1ff3b2
nikibhatt <nkibhatt@users.noreply.github.com>
2020-05-08 10:02:14 -0700

    completed

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,10 +1,14 @@
 def get_indices_of_item_weights(weights, length, limit):
     cache = {}
-    sum = 0
+
+    if length == 2 and weights[0] + weights[1] == limit:
+        return (1,0)
+
     for i in range(length):
 
         if weights[i] not in cache:
             cache[weights[i]] = i
+
             if limit-weights[i] in cache:
                 if cache[limit-weights[i]] >= cache[weights[i]]:
                     return (cache[limit-weights[i]], cache[weights[i]])
@@ -12,3 +16,6 @@ def get_indices_of_item_weights(weights, length, limit):
                     return (cache[weights[i]], cache[limit-weights[i]])
 
     return None
+
+weights_2 = [4, 4]
+print(get_indices_of_item_weights(weights_2, 2, 8))
